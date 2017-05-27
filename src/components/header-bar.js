c578f0aa6c45ed12192bcacb3c4652bddcb2e52b
Seshadri Mahalingam <seshadri@berkeley.edu>
2017-05-27 13:01:20 -0700

    s/search/undo+redo

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
 
-import ActionSearch from 'material-ui/svg-icons/action/search';
+import AppBar from 'material-ui/AppBar';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
+
+import ContentUndo from 'material-ui/svg-icons/content/undo';
+import ContentRedo from 'material-ui/svg-icons/content/redo';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 const HeaderBar = () => (
   <AppBar
     title="Pandamic 🐼😰🐛"
     showMenuIconButton={false}
-    iconElementRight={<IconButton><ActionSearch /></IconButton>}
-  />
+    iconElementRight={
+        <IconMenu
+          iconButtonElement={
+            <IconButton><MoreVertIcon /></IconButton>
+          }
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+        >
+        <MenuItem primaryText="Undo" leftIcon={<ContentUndo />} />
+        <MenuItem primaryText="Redo" leftIcon={<ContentRedo />} disabled={true} />
+      </IconMenu>
+    }
+  >
+  </AppBar>
 )
 
 export default HeaderBar;
